e4b449c3955d249661291f890db7b1cca4985b3d
Stratix <darnokusex@wp.pl>
2018-12-03 21:18:48 +0100

    przydatne skrypty

--- minix-files/copy_from_minix_usr.sh
+++ minix-files/copy_from_minix_usr.sh
@@ -1,2 +1,3 @@
-mkdir src/
-cp -f -r ../minix-toolkit/minix_usr/src/ src/ 
+cp -f -r ../minix-toolkit/minix_usr/src ./ 
+cp -f -r ../minix-toolkit/minix_usr/include ./ 
+rm -f -r src/*/*.o
--- minix-files/copy_to_minix_usr.sh
+++ minix-files/copy_to_minix_usr.sh
@@ -1,1 +1,2 @@
-sudo cp -f -r src/ ../minix-toolkit/minix_usr/src/
+sudo cp -f -r src ../minix-toolkit/minix_usr/
+sudo cp -f -r include ../minix-toolkit/minix_usr/
