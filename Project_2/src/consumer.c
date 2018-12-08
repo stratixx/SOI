5bc3edddafd7228c9305211a79899f0edfcbab06
Stratix <darnokusex@wp.pl>
2018-12-09 00:52:05 +0100

    stworzone pliki projektu 2 razem z makefile

---
+++ Project_2/src/consumer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#include "../inc/consumer.h"
+#include "../inc/tools.h"
+
+int main (int argc, char* argv[])
+{
+    display("consumer");
+
+
+    return 0;
+}
---
+++ Project_2/src/producer.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#include "../inc/producer.h"
+#include "../inc/tools.h"
+
+int main (int argc, char* argv[])
+{
+    display("producer");
+
+    return 0;
+}
